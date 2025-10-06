7d7139c5185b66414dfc0d841d459fa5c1869891
Mohamed Walid <mohamed.wmahdi@gmail.com>
2025-10-06 15:07:41 +0300

    Add Bla.js with student data

---
+++ Bla.js
@@ -0,0 +1,23 @@
+const student = [
+  {
+    name : 'Mostafa',
+    gpa : '0.7'
+  },
+
+  {
+    name : 'Mohamed',
+    gpa : '0.9'
+  },
+
+  {
+    name : 'Lana',
+    gpa : '0.75',
+  }
+];
+
+console.log(student[1].name);
+
+
+
+
+
